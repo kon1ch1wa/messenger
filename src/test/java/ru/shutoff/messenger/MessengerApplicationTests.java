2274a01d3559cb5bf1367fb6fef6ee6587efae36
Yaroslav Shutov <yarshutov@yandex.ru>
2023-08-30 22:23:26 +0300

    init commit

---
+++ src/test/java/ru/shutoff/messenger/MessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shutoff.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
